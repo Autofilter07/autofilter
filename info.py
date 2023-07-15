d874e8993b170ecfc277acf32885580202384f41
Autofilter09 <139616230+Autofilter09@users.noreply.github.com>
2023-07-16 00:03:41 +0530

    Update info.py

--- info.py
+++ info.py
@@ -64,7 +64,7 @@ LOG_STR += ("Spell Check Mode Is Enabled, bot will be suggesting related movies 
 LOG_STR += (f"MAX_LIST_ELM Found, long list will be shortened to first {MAX_LIST_ELM} elements\n" if MAX_LIST_ELM else "Full List of casts and crew will be shown in imdb template, restrict them by adding a value to MAX_LIST_ELM\n")
 LOG_STR += f"Your current IMDB template is {IMDB_TEMPLATE}"
 
-UPSTREAM_REPO = environ.get('UPSTREAM_REPO', 'https://github.com/TamilanBotsZ/AwesomeFilterPro')
+
 
 AUTO_DELETE_SECONDS = int(environ.get('AUTO_DELETE_SECONDS', 300))
 AUTO_DELETE = environ.get('AUTO_DELETE', True)
@@ -72,6 +72,6 @@ if AUTO_DELETE == "True":
     AUTO_DELETE = True
 
 #Sample
-SHORTNER_SITE = ""
-SHORTNER_API = ""
+SHORTNER_SITE = environ.get('URL_SHORTNER_SITE','omegalinks.in')
+SHORTNER_API = environ.get('URL_SHORTNER_API','9cd702e495f6391ac80b8ce81fc3100c62afa43f')
 
