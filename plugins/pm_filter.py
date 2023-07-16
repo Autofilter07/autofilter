43e90178e5665c2b278dee7ac4737c2bf07ef268
Autofilter09 <139616230+Autofilter09@users.noreply.github.com>
2023-07-17 00:42:30 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -106,7 +106,7 @@ async def next_page(bot, query):
         ]
     btn.insert(0, 
         [
-            InlineKeyboardButton('🔋 Hᴏᴡ ᴛᴏ ᴅᴏᴡɴʟᴏᴀᴅ 🔋', url='https://t.me/c/1506384821/4')
+            InlineKeyboardButton('🔋 Hᴏᴡ ᴛᴏ ᴅᴏᴡɴʟᴏᴀᴅ 🔋', url='https://t.me/howtodownl0admovie/4')
         ]
     )
     btn.insert(1, 
